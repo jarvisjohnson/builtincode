b94a270c53086e64ee753a04fe9ac8450e99f789
Jarvis Johnson <jarviscjohnson@gmail.com>
2017-04-28 16:48:52 -0400

    reinstall react, put in example config

--- app/react/index.js
+++ app/react/index.js
@@ -1,6 +1,9 @@
 import RWR from 'react-webpack-rails';
 RWR.run();
 
+import HelloWorld from './components/hello-world';
+RWR.registerComponent('HelloWorld', HelloWorld);
+
 import PageTitle from './components/page-title';
 RWR.registerComponent('PageTitle', PageTitle);
 
