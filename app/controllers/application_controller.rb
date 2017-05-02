a136a1e62345274b509f606229cbb9de5a8638fa
Jarvis Johnson <jarviscjohnson@gmail.com>
2017-05-02 10:41:45 -0400

    debugging ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_client!
-  protect_from_forgery prepend: true
+  protect_from_forgery with: :exception
   # before_action :set_client
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_support_receiver
