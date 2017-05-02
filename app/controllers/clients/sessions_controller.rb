0614f027707be669fbc32fd637012f53ed19bc9a
Jarvis Johnson <jarviscjohnson@gmail.com>
2017-05-02 11:25:37 -0400

    debugging ssl

--- app/controllers/clients/sessions_controller.rb
+++ app/controllers/clients/sessions_controller.rb
@@ -2,7 +2,7 @@ class Clients::SessionsController < Devise::SessionsController
 
   before_filter :remove_authentication_flash_message_if_root_url_requested
 
-  prepend_before_filter :verify_client, only: [:destroy]
+  # prepend_before_filter :verify_client, only: [:destroy]
 
   private
 
@@ -13,9 +13,9 @@ class Clients::SessionsController < Devise::SessionsController
     raise
   end
 
-  def verify_client
-    ## redirect to appropriate path
-    redirect_to new_client_session_path, notice: 'You have signed out. Please sign in again.' and return unless client_signed_in?
-  end  
+  # def verify_client
+  #   ## redirect to appropriate path
+  #   redirect_to new_client_session_path, notice: 'You have signed out. Please sign in again.' and return unless client_signed_in?
+  # end  
 
 end
