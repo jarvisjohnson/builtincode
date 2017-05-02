34d12217868fe2cb2119e711e70f0d87b6301ed7
Jarvis Johnson <jarviscjohnson@gmail.com>
2017-05-02 12:23:07 -0400

    fix images in emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Devise Mailer
-  config.action_mailer.default_url_options = { :host => "builtincode.com" }
+  config.action_mailer.default_url_options = { :host => ENV["site_url"] }
 
   # Images in emails
   config.action_mailer.asset_host = ENV["site_url"]
